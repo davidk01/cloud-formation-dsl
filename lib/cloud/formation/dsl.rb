f3d59070d10f765e6deb148585063e86dab4a80c
david karapetyan <david@david-Dell-System-XPS-L322X.buildd>
2013-08-24 03:29:42 -0700

    prototype example of pool definitions

--- lib/cloud/formation/dsl.rb
+++ lib/cloud/formation/dsl.rb
@@ -1,9 +1,16 @@
-require "cloud/formation/dsl/version"
+require 'cloud/formation/dsl/version'
+require 'pegrb'
 
 module Cloud
-  module Formation
-    module Dsl
-      
-    end
+module Formation
+module Dsl
+
+  @grammar = Grammar.rules do
+    
   end
+
+  def self.parse(str); @grammar.parse(str); end
+
+end
+end
 end
