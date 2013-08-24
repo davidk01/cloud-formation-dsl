40726cb6a6e2c3aa32c19328ad45a8f19a6ac0a5
david karapetyan <david@david-Dell-System-XPS-L322X.(none)>
2013-08-24 13:22:50 -0700

    comments and stuff

--- lib/cloud/formation/dsl.rb
+++ lib/cloud/formation/dsl.rb
@@ -1,5 +1,6 @@
 #require 'cloud/formation/dsl/version'
 require 'pegrb'
+require 'pathname'
 
 module Cloud
 module Formation
@@ -17,6 +18,8 @@ module Dsl
     ws, newline = one_of(' ', "\t"), one_of("\n", "\r\n", "\r")
     comma_space = (one_of(',') > ws.many.any).ignore
 
+    comment = (one_of('#') > (wildcard > !newline).many.any > wildcard > newline.many).ignore
+
     # used for port specifications
     integer = (one_of(/[1-9]/)[:first] > one_of(/\d/).many.any[:rest]) >> ->(s) {
       [Integer.new((s[:first][0].text + s[:rest].map(&:text).join).to_i)]
@@ -26,18 +29,21 @@ module Dsl
     }
 
     # used for various values in pool definition blocks
-    name = (one_of(/[a-zA-Z]/).many > (one_of('-') > 
-     one_of(/[a-zA-Z]/).many).many.any)[:n] >> ->(s) {
+    name = (one_of(/[a-zA-Z]/).many > (one_of('-', ' ') > 
+     one_of(/[a-zA-Z0-9_\. ]/).many).many.any)[:n] >> ->(s) {
       [Name.new(s[:n].map(&:text).join)]
     }
     name_list = (name[:head] > (comma_space > name).many.any[:tail]) >> ->(s) {
       [NameList.new(s[:head] + s[:tail])]
     }
 
-    # some values need to be filepaths
-    absolute_paths =
+    # some values need to be absolute paths
+    path_sep = one_of('/').ignore
+    absolute_path = (path_sep > name[:head] > (path_sep > name).many.any[:tail]) >> ->(s) {
+      [Pathname.new('/' + s[:head][0].value + '/' + s[:tail].map(&:value).join('/'))]
+    }
 
-    rule :start, name_list | integer_list
+    rule :start, comment
   end
 
   def self.parse(str); @grammar.parse(str); end
