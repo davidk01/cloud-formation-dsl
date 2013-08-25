d5bfdbd0d0acceb9208c11f582455a1a4f459f90
david karapetyan <david@david-Dell-System-XPS-L322X.(none)>
2013-08-25 15:29:05 -0700

    grammar now successfully parses the example formation. time to implement AST transformations

--- lib/cloud/formation/dsl.rb
+++ lib/cloud/formation/dsl.rb
@@ -10,33 +10,42 @@ module Dsl
   class PairNode < Struct.new(:key, :value); end
   class Integer < ValueNode; end
   class IntegerList < ValueNode; end
-  class Name < ValueNode; end
+  class Key < ValueNode; end
   class PairList < ValueNode; end
   class QuotedValue < ValueNode; end
+  class QuotedValueList < ValueNode; end
   class VMSpec < Struct.new(:name, :count, :image_name); end
+  class NamedBootstrapSequence < Struct.new(:name, :sequence); end
+  class BootstrapSequenceList < ValueNode; end
+  class PoolDefinition < Struct.new(:vm_spec, :flavor, :ports, :bootstrap_sequence); end
+  class PoolDefinitionList < ValueNode; end
+  class BoxDefinition < Struct.new(:vm_spec, :falvor, :bootstrap_sequence); end
+  class BoxDefinitionList < ValueNode; end
+  class Defaults < ValueNode; end
+
+  def self.listify(expr, sep, node_type)
+    (expr[:first] > (sep.ignore > expr).many.any[:rest]) >> ->(s) {
+      [node_type.new(s[:first] + s[:rest])]
+    }
+  end
 
   @grammar = Grammar.rules do
     # fundamental building blocks
     ws, newline = one_of(' ', "\t"), one_of("\n", "\r\n", "\r")
-    comma_space = (one_of(',') > ws.many.any).ignore
-
     comment = (one_of('#') > (wildcard > !newline).many.any > wildcard > newline.many).ignore
 
     # used for port specifications
     integer = (one_of(/[1-9]/)[:first] > one_of(/\d/).many.any[:rest]) >> ->(s) {
       [Integer.new((s[:first][0].text + s[:rest].map(&:text).join).to_i)]
     }
-    integer_list = (integer[:head] > (comma_space > integer).many.any[:tail]) >> ->(s) {
-      [IntegerList.new(s[:head] + s[:tail])]
-    }
+    integer_list = Dsl::listify(integer, m(', '), IntegerList)
 
-    # used for various values in pool definition blocks
-    name = (one_of(/[a-zA-Z]/).many > (one_of('-', ' ') > 
+    # key, value definitions
+    key = (one_of(/[a-zA-Z]/).many > (one_of('-', ' ') > 
      one_of(/[a-zA-Z0-9_\. ]/).many).many.any)[:n] >> ->(s) {
-      [Name.new(s[:n].map(&:text).join)]
+      [Key.new(s[:n].map(&:text).join)]
     }
 
-    # some values need to be absolute paths and some need to be bash command sequences
     double_quoted_value = (one_of('"') > ((wildcard > !one_of('"')).many.any >
      wildcard)[:quoted_value] > one_of('"')) >> ->(s) {
       [QuotedValue.new(s[:quoted_value].map(&:text).join)]
@@ -46,41 +55,65 @@ module Dsl
      [QuotedValue.new(s[:quoted_value].map(&:text).join)]
     }
     quoted_value = single_quoted_value | double_quoted_value
-    quoted_value_list = quoted_value[:first] >
-     (m(', ').ignore > quoted_value).many.any[:rest] >> ->(s) {
-      [QuotedValueList.new(s[:first] + s[:rest])]
-    }
+    quoted_value_list = Dsl::listify(quoted_value, m(', '), QuotedValueList)
 
-    generic_pair = (ws.many.any > name[:key] > m(': ') > quoted_value_list[:value]) >> ->(s) {
+    # pair, pair list definitions
+    generic_pair = (ws.many.any > key[:key] > m(': ') > quoted_value_list[:value]) >> ->(s) {
       [PairNode.new(s[:key].first, s[:value].first)]
     }
-    generic_pair_list = (generic_pair[:first] >
-     (newline.ignore > generic_pair).many.any[:rest]) >> ->(s) {
-      [PairList.new(s[:first] + s[:rest])]
-    }
+    generic_pair_list = Dsl::listify(generic_pair, newline, PairList)
 
+    # vm spec requires certain common things
     vm_spec = (quoted_value[:name] > m(', ') > integer[:count] >
      (m(' instance ') | m(' instances ')) > m('with ') > quoted_value[:image_name]) >> ->(s) {
       [VMSpec.new(s[:name].first, s[:count].first, s[:image_name].first)]
     }
  
-    #TODO: named bootstrap sequence
+    #named bootstrap sequences
+    named_bootstrap_sequence = (m('bootstrap sequence: ') > quoted_value[:sequence_name] >
+     newline > generic_pair_list[:sequence]) >> ->(s) {
+      [NamedBootstrapSequence.new(s[:sequence_name].first, s[:sequence].first)]
+    }
+    named_bootstrap_sequence_list = Dsl::listify(named_bootstrap_sequence,
+     newline.many, BootstrapSequenceList)
 
     # pool definition blocks
     pool_def_block = (m('pool: ') > vm_spec[:vm_spec] > newline >
      ws.many.any > m('vm flavor: ') > quoted_value[:flavor_name] > newline >
      (ws.many.any > m('service ports: ') > integer_list[:ports] > newline).any >
-     ws.many.any > m('bootstrap sequence:') > newline > generic_pair_list) >> ->(s) {
-      [PoolDef.new]
+     ws.many.any > m('bootstrap sequence:') > newline >
+     generic_pair_list[:bootstrap_sequence]) >> ->(s) {
+      [PoolDefinition.new(s[:vm_spec].first, 
+       s[:flavor_name].first, (s[:ports] || []).first, 
+       s[:bootstrap_sequence].first)]
     }
-    pool_def_block_list = (pool_def_block[:first] >
-     (newline.ignore > pool_def_block).many.any[:rest]) >> ->(s) {
-      [PoolDefList.new(s[:first] + s[:rest])]
+    pool_def_block_list = Dsl::listify(pool_def_block, newline.many, PoolDefinitionList)
+
+    #box definition blocks
+    box_def_block = (m('box: ') > vm_spec[:vm_spec] > newline >
+     ws.many.any > m('vm flavor: ') > quoted_value[:flavor_name] > newline >
+     ws.many.any > m('bootstrap sequence:') > newline >
+     generic_pair_list[:bootstrap_sequence]) >> ->(s) {
+      [BoxDefinition.new(s[:vm_spec].first, s[:flavor_name].first,
+       s[:bootstrap_sequence].first)]
+    }
+    box_def_block_list = Dsl::listify(box_def_block, newline.many, BoxDefinitionList)
+
+    # default key, value list
+    defaults = (m('defaults:') > newline > generic_pair_list[:defaults]) >> ->(s) {
+      [Defaults.new(s[:defaults])]
     }
 
-    #TODO: box definition sequence
+    # so a cloud formation spec is just a default section, followed by named bootstrap
+    # sequences, followed by some pool definitions and then followed by some box definitions
+    # theoretically the entire thing can be empty
+    rule :start, ((defaults[:defaults] > newline.many).any >
+     (named_bootstrap_sequence_list[:bootstrap_sequence] > newline.many).any >
+     (pool_def_block_list[:pool_definitions] > newline.many).any >
+     box_def_block_list.any[:box_definitions]) >> ->(s) {
+      require 'pry'; binding.pry
+    }
 
-    rule :start, pair_list
   end
 
   def self.parse(str); @grammar.parse(str); end
