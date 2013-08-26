0b1e2f1cc606e744eaad2354c2ae55a12385cf2f
david karapetyan <david@david-Dell-System-XPS-L322X.(none)>
2013-08-26 12:34:27 -0700

    modulraize

--- lib/dsl.rb
+++ lib/dsl.rb
@@ -1,25 +1,7 @@
-#require 'cloud/formation/dsl/version'
-require 'pegrb'
+require 'dsl/version'
+require 'dsl/grammar'
 
-module Cloud
-module Formation
 module Dsl
-
-  # AST nodes
-  class ValueNode < Struct.new(:value); end
-  class PairNode < Struct.new(:key, :value); end
-  class Integer < ValueNode; end
-  class IntegerList < ValueNode; end
-  class PairList < ValueNode; end
-  class ValueList < ValueNode; end
-  class VMSpec < Struct.new(:name, :count, :image_name); end
-  class NamedBootstrapSequence < Struct.new(:name, :sequence); end
-  class NamedBootstrapSequenceList < ValueNode; end
-  class PoolDefinition < Struct.new(:vm_spec, :flavor, :ports, :bootstrap_sequence); end
-  class PoolDefinitionList < ValueNode; end
-  class BoxDefinition < Struct.new(:vm_spec, :falvor, :bootstrap_sequence); end
-  class BoxDefinitionList < ValueNode; end
-  class Defaults < ValueNode; end
 
   ##
   # This is the final result of the parsing process and contains all the necessary
@@ -53,105 +35,6 @@ module Dsl
 
   end
 
-  def self.listify(expr, sep, node_type)
-    (expr[:first] > (sep.ignore > expr).many.any[:rest]) >> ->(s) {
-      [node_type.new(s[:first] + s[:rest])]
-    }
-  end
+  def self.parse(str); Grammar.parse(str); end
 
-  @grammar = Grammar.rules do
-    # fundamental building blocks
-    ws, newline = one_of(' ', "\t"), one_of("\n", "\r\n", "\r")
-    comment = (one_of('#') > (wildcard > !newline).many.any > wildcard > newline.many).ignore
-
-    # used for port specifications
-    integer = (one_of(/[1-9]/)[:first] > one_of(/\d/).many.any[:rest]) >> ->(s) {
-      [Integer.new((s[:first][0].text + s[:rest].map(&:text).join).to_i)]
-    }
-    integer_list = Dsl::listify(integer, m(', '), IntegerList)
-
-    # key, value definitions
-    key = (one_of(/[a-zA-Z]/).many > (one_of('-', ' ') > 
-     one_of(/[a-zA-Z0-9_\. ]/).many).many.any)[:n] >> ->(s) {
-      [s[:n].map(&:text).join]
-    }
-    double_quoted_value = (one_of('"') > ((wildcard > !one_of('"')).many.any >
-     wildcard)[:quoted_value] > one_of('"')) >> ->(s) {
-      [s[:quoted_value].map(&:text).join]
-    }
-    single_quoted_value = (one_of("'") > ((wildcard > !one_of("'")).many.any >
-     wildcard)[:quoted_value] > one_of("'")) >> ->(s) {
-     [s[:quoted_value].map(&:text).join]
-    }
-    quoted_value = single_quoted_value | double_quoted_value
-    quoted_value_list = Dsl::listify(quoted_value, m(', '), ValueList)
-
-    # pair, pair list definitions
-    generic_pair = (ws.many.any > key[:key] > m(': ') > quoted_value_list[:value]) >> ->(s) {
-      [PairNode.new(s[:key].first, s[:value].first)]
-    }
-    generic_pair_list = Dsl::listify(generic_pair, newline, PairList)
-
-    # vm spec requires certain common things
-    vm_spec = (quoted_value[:name] > m(', ') > integer[:count] >
-     (m(' instance ') | m(' instances ')) > m('with ') > quoted_value[:image_name]) >> ->(s) {
-      [VMSpec.new(s[:name].first, s[:count].first, s[:image_name].first)]
-    }
- 
-    #named bootstrap sequences
-    named_bootstrap_sequence = (m('bootstrap sequence: ') > quoted_value[:sequence_name] >
-     newline > generic_pair_list[:sequence]) >> ->(s) {
-      [NamedBootstrapSequence.new(s[:sequence_name].first, s[:sequence].first)]
-    }
-    named_bootstrap_sequence_list = Dsl::listify(named_bootstrap_sequence,
-     newline.many, NamedBootstrapSequenceList)
-
-    # pool definition blocks
-    pool_def_block = (m('pool: ') > vm_spec[:vm_spec] > newline >
-     ws.many.any > m('vm flavor: ') > quoted_value[:flavor_name] > newline >
-     (ws.many.any > m('service ports: ') > integer_list[:ports] > newline).any >
-     ws.many.any > m('bootstrap sequence:') > newline >
-     generic_pair_list[:bootstrap_sequence]) >> ->(s) {
-      [PoolDefinition.new(s[:vm_spec].first, 
-       s[:flavor_name].first, (s[:ports] || []).first, 
-       s[:bootstrap_sequence].first)]
-    }
-    pool_def_block_list = Dsl::listify(pool_def_block, newline.many, PoolDefinitionList)
-
-    #box definition blocks
-    box_def_block = (m('box: ') > vm_spec[:vm_spec] > newline >
-     ws.many.any > m('vm flavor: ') > quoted_value[:flavor_name] > newline >
-     ws.many.any > m('bootstrap sequence:') > newline >
-     generic_pair_list[:bootstrap_sequence]) >> ->(s) {
-      [BoxDefinition.new(s[:vm_spec].first, s[:flavor_name].first,
-       s[:bootstrap_sequence].first)]
-    }
-    box_def_block_list = Dsl::listify(box_def_block, newline.many, BoxDefinitionList)
-
-    # default key, value list
-    defaults = (m('defaults:') > newline > generic_pair_list[:defaults]) >> ->(s) {
-      [Defaults.new(s[:defaults])]
-    }
-
-    # so a cloud formation spec is just a default section, followed by named bootstrap
-    # sequences, followed by some pool definitions and then followed by some box definitions
-    # theoretically the entire thing can be empty
-    rule :start, ((defaults[:defaults] > newline.many).any >
-     (named_bootstrap_sequence_list[:named_bootstrap_sequences] > newline.many).any >
-     (pool_def_block_list[:pool_definitions] > newline.many).any >
-     box_def_block_list.any[:box_definitions]) >> ->(s) {
-      RawCloudFormation.new(s[:defaults].first, s[:named_bootstrap_sequences].first,
-       s[:pool_definitions].first, s[:box_definitions].first)
-    }
-
-  end
-
-  def self.parse(str); @grammar.parse(str); end
-
-  def self.pipeline(raw_ast)
-    raw_ast.resolve_bootstrap_sequence_includes
-  end
-
-end
-end
 end
---
+++ lib/dsl/ast.rb
@@ -0,0 +1,19 @@
+module Dsl
+
+  # AST nodes
+  class ValueNode < Struct.new(:value); end
+  class PairNode < Struct.new(:key, :value); end
+  class Integer < ValueNode; end
+  class IntegerList < ValueNode; end
+  class PairList < ValueNode; end
+  class ValueList < ValueNode; end
+  class VMSpec < Struct.new(:name, :count, :image_name); end
+  class NamedBootstrapSequence < Struct.new(:name, :sequence); end
+  class NamedBootstrapSequenceList < ValueNode; end
+  class PoolDefinition < Struct.new(:vm_spec, :flavor, :ports, :bootstrap_sequence); end
+  class PoolDefinitionList < ValueNode; end
+  class BoxDefinition < Struct.new(:vm_spec, :falvor, :bootstrap_sequence); end
+  class BoxDefinitionList < ValueNode; end
+  class Defaults < ValueNode; end
+
+end
